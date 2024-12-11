16e31fbc60fde85521026371d07deb09d12e5b59
JinuVarghese9490 <varghesejinu895@gmail.com>
2024-12-11 08:20:42 +0530

    fifth

--- 7rmart_Supermarket/src/main/java/constants/Constants.java
+++ 7rmart_Supermarket/src/main/java/constants/Constants.java
@@ -1,6 +1,7 @@
 package constants;
 
 public class Constants {
-	public static final String TESTDATAFILE = System.getProperty("user.dir")+ "\\src\\test\\resources\\TestData (2).xlsx";
+	public static final String TESTDATAFILE = System.getProperty("user.dir")+ "\\src\\test\\resources\\TestData (1).xlsx";
 	public static final String TESTDATAPIC = System.getProperty("user.dir")+ "\\src\\test\\resources\\images.jpeg";
+	public static final String CONFIGFILE = System.getProperty("user.dir")+ "\\src\\main\\resources\\config.properties";
 }
--- 7rmart_Supermarket/src/test/java/testscript/SubCategoryTest.java
+++ 7rmart_Supermarket/src/test/java/testscript/SubCategoryTest.java
@@ -1,30 +1,28 @@
-package testscript;
+/*package testscript;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import pages.LoginPage;
 import pages.SubCategoryPage;
 import utility.ExcelUtility;
 
 public class SubCategoryTest extends Base {
-	@Test(description="This is for adding an item with image to sub category list")
+	@Test(description="This is for adding category and sub category with image to sub category list"
+			,retryAnalyzer=retry.Retry.class)
 
 	public void verifyUserAbletoAddaNewItemtoTheSubCategoryList() {
 		
 		String userName =ExcelUtility.getString(1, 0,"LoginPage");
 		String password = ExcelUtility.getString(1, 1, "LoginPage");
+		LoginPage loginpage = new LoginPage(driver);
+		loginpage.enterUserNameOnUserNameField(userName).enterPasswordOnPasswordField(password)
+		.clickonSigninButton();
 		SubCategoryPage subcategorypage = new SubCategoryPage(driver);
-		subcategorypage.enterUserNameOnUserNameField(userName);
-		subcategorypage.enterPasswordOnPasswordField(password);
-		subcategorypage.clickonSigninButton();
-        subcategorypage.clickonMoreInfoButton();
-        
-        subcategorypage.clickonEditButton();
-        subcategorypage.dropDownSubCategory();
-        subcategorypage.clickonChooseFile();
-        subcategorypage.scrollDown();
-        subcategorypage.clickonUpdateButton();
+        subcategorypage.clickonMoreInfoButton().clickonEditButton().dropDownSubCategory().enterSubCategoryField()
+		.clickonChooseFile().scrollDown().clickonUpdateButton();
         boolean isSubcategoryCrestedSuccessfully = subcategorypage.isSuccessAlertAvailable();
-		Assert.assertTrue(isSubcategoryCrestedSuccessfully, "Sub Category already exists");
+		Assert.assertTrue(isSubcategoryCrestedSuccessfully, "Sub Category updation Failed");
 	}
 
-}
+}*/
